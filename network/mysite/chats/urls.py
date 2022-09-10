b6eec81ebc3661a1bf097575db530a0717ea7733
ramses314 <forwork31415@gmail.com>
2022-09-10 11:10:59 +0700

    test

--- network/mysite/chats/urls.py
+++ network/mysite/chats/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path, include
 
+from chats.views import plakat
+
 app_name = 'chats'
 
 urlpatterns = [
-
-
+    path('', plakat, name='plakat')
 ]
--- network/mysite/chats/views.py
+++ network/mysite/chats/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def plakat(request):
+
+    print(1111)
+
+    return render(request, 'chats/plakat.html')
